834c0627b13ab190021c6567b6b32361a4a56248
jspe2ba03 <jspe2ba03@scs.local>
2025-03-12 10:36:55 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  preferences = {
+    option1: false,
+    option2: false,
+    option3: false,
+    rangeValue: 50,
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+    radio1: false,
+    radio2: false
+  };
 
   constructor() {}
 
+  ngOnInit() {
+    this.loadPreferences();
+  }
+
+  async loadPreferences() {
+    const { value } = await Preferences.get({ key: 'appSettings' });
+    if (value) {
+      this.preferences = JSON.parse(value);
+    }
+  }
+
+  async savePreferences() {
+    await Preferences.set({
+      key: 'appSettings',
+      value: JSON.stringify(this.preferences),
+    });
+    alert('Settings saved!');
+  }
+
+  async resetPreferences() {
+    this.preferences = {
+      option1: false,
+      option2: false,
+      option3: false,
+      rangeValue: 50,
+      checkbox1: false,
+      checkbox2: false,
+      checkbox3: false,
+      radio1: false,
+      radio2: false
+
+    };
+    await Preferences.remove({ key: 'appSettings' });
+    alert('Settings reset to default!');
+  }
+
+  onPreferenceChange() {
+    this.savePreferences();
+  }
 }
